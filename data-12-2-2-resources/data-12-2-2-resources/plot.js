2119416d2dd2b00fec4cfcbba14cd33e3fdc80ea
xher95 <hanabi.xe@gmail.com>
2020-10-24 21:31:54 -0500

    Deployment

---
+++ data-12-2-2-resources/data-12-2-2-resources/plot.js
@@ -0,0 +1,44 @@
+// Sort and select cities
+var sortedCities = cityGrowths.sort((a,b) =>
+a.Increase_from_2016 - b.Increase_from_2016).reverse();
+
+var topFiveCities = sortedCities.slice(0,5);
+
+// Arrays of city names and growth figures
+var topFiveCityNames = topFiveCities.map(city => city.City);
+var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
+
+// Create bar chart with arrays
+var trace = {
+    x: topFiveCityNames,
+    y: topFiveCityGrowths,
+    type: "bar"
+  };
+  var data = [trace];
+  var layout = {
+    title: "Most Rapidly Growing Cities",
+    xaxis: { title: "City" },
+    yaxis: { title: "Population Growth, 2016-2017"}
+  };
+  Plotly.newPlot("bar-plot", data, layout);
+};
+
+function optionChanged(newSample) {
+  console.log(newSample);
+}
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    PANEL.html("");
+    PANEL.append("h6").text(result.location);
+  });
+}
